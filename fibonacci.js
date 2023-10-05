f86da316ffcebf4f98cfcd1ddf731418984738e1
KATE_FISCHER <KATME2006@GMAIL.COM>
2023-10-04 19:29:07 -0500

    Fibonnaci Sequence Algorithm with all tests passed.

--- fibonacci.js
+++ fibonacci.js
@@ -1,5 +1,20 @@
 function fibonacci(num) {
-  return 0;
+  //Array to begin Fibonacci Sequence
+  fibArr = [0, 1];
+
+  if (num === 1||num ===0) {
+    return fibArr[0];
+  } else {
+    //Loop to adding together two numbers and replacing last element in array
+    for (i = 2; i < num; i++) {
+      fibArr.push(fibArr[0] + fibArr[1]);
+      fibArr.shift();
+    }
+  }
+
+  const fibNum = fibArr[0] + fibArr[1];
+
+  return fibNum;
 }
 
 module.exports = fibonacci;
